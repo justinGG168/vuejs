5f06ccda0ecb3d3fec5b83bd970a9d3f824829b0
justinGG168 <bolyck@gmail.com>
2024-03-05 10:28:17 +0800

    feat: first

---
+++ vue3-demo/src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+  </WelcomeItem>
+  <WelcomeItem>
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a>
+  </WelcomeItem>
+</template>
