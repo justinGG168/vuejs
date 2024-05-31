89c609a1b445153a7701521c06e5d24144d26be3
justinGG168 <bolyck@gmail.com>
2024-05-31 13:44:04 +0800

    feat: 技巧1

---
+++ vue3-demo/src/views/5-31.demo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  在vue2中v-for的优先级高于v-if，而在vue3中v-if的优先级要高于v-for-->
+<!--  如何v-for和v-if同时使用，需要computed -->
+</template>
+
+<style scoped>
+
+</style>
