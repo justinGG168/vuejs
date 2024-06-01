52ca397bbcbf401552b1994e52654185dedccff1
justinGG168 <bolyck@gmail.com>
2024-06-01 23:55:44 +0800

    feat: 技巧1

---
+++ vue3-demo/src/views/6-17.demo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  注意：1.当设置“auto-upload”为false时，beforeUpload、onSuccess、before-upload、on-success都不会触发，此时只会触发on-change；-->
+
+<!--  2.当设置“limit”为1时，on-change之后触发一次，on-change会随 limit 改变而改变，另外，当上传的文件超过limit时，会触发on-exceed方法；-->
+</template>
+
+<style scoped>
+
+</style>
